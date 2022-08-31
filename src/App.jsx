3c0be7818b475b5f3c92ceea2ffbe8cd07eeee0a
Oyebamiji Alex <alexferanmi390@gmail.com>
2022-08-31 10:28:44 +0100

    fix:navbar

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ import FooterLink from "./components/FooterLink";
 
 const App = () => {
   return (
-    <main className="bg-primary h-full px-6 lg:px-12 font-poppins">
+    <main className="bg-primary h-full px-6 md:px-[4rem] font-poppins">
       <Navbar />
       <Header />
       <Stats />

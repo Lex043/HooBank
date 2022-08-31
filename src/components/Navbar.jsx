2684f3a951fa58d1a83adaaafc30eee843827f60
Oyebamiji Alex <alexferanmi390@gmail.com>
2022-08-31 10:05:12 +0100

    fix:navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
   const navRef = useRef();
 
   const showNavbar = () => {
-    navRef.current.classList.remove("translate-y-[-100vh]");
+    navRef.current.classList.toggle("translate-y-[-100vh]");
   };
 
   return (

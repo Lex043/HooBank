cab045ba1c42bc231ca9f5e5f2fddbb006aa9e15
Oyebamiji Alex <alexferanmi390@gmail.com>
2023-04-05 19:52:07 +0100

    remove preloader

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,3 @@
-import { useEffect, useRef, useState } from "react";
-
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
 import Stats from "./components/Stats";
@@ -17,51 +15,22 @@ import scroll from "./components/animations/Scroll";
 scroll();
 
 const App = () => {
-  const [preloader, setPreloader] = useState(true);
-
-  const [timer, setTimer] = useState(4);
-
-  const id = useRef(null);
-
-  const clear = () => {
-    window.clearInterval(id.current);
-    setPreloader(false);
-  };
-
-  useEffect(() => {
-    id.current = window.setInterval(() => {
-      setTimer((timer) => timer - 1);
-    }, 1000);
-  }, [timer]);
-
-  useEffect(() => {
-    if (timer === 0) {
-      clear();
-    }
-  }, [timer]);
-
   return (
     <>
-      {preloader ? (
-        <div className="flex flex-col items-center justify-center bg-black fixed top-0 right-0 bottom-0 left-0 z-10">
-          <h1 className="text-3xl text-white">HOOBANK</h1>
-        </div>
-      ) : (
-        <main className="bg-primary h-full px-6 md:px-[4rem] font-poppins">
-          <Navbar />
-          <Header />
-          <Stats />
-          <HandleMoney />
-          <ControlBilling />
-          <BetterCard />
-          <Review />
-          <Comments />
-          <Partners />
-          <TryService />
-          <Footer />
-          <FooterLink />
-        </main>
-      )}
+      <main className="bg-primary h-full px-6 md:px-[4rem] font-poppins">
+        <Navbar />
+        <Header />
+        <Stats />
+        <HandleMoney />
+        <ControlBilling />
+        <BetterCard />
+        <Review />
+        <Comments />
+        <Partners />
+        <TryService />
+        <Footer />
+        <FooterLink />
+      </main>
     </>
   );
 };
